26e6d918168b3a7450fed7a13bdef8133001898d
jakkrit aryamuang <jakkrit.arya@mail.kmutt.ac.th>
2018-05-25 19:24:59 +0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using system;
+
+namespace VisualStudioCsharpCode 
+{
+   public class Program
+   {
+       public static void Main(string[] args)
+       {
+           Console.WriteLine("Hello to try typing c# code in visual studio code");
+       }
+   }
+}
